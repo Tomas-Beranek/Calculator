59c5008e8a2fd10e03003eb11bc6ddf4dffccc68
Tomas.Beranek <beranek.t@outlook.cz>
2023-06-18 22:17:11 +0200

    calculator first commit -basic numpad -basic mvvm architecture

---
+++ view/UserControls/numberPad.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calc.view.UserControls
+{
+    /// <summary>
+    /// Interakční logika pro numberPad.xaml
+    /// </summary>
+    public partial class numberPad : UserControl
+    {
+        public numberPad()
+        {
+            InitializeComponent();
+            btnNum.FontSize = 25;
+        }
+        private string contentFill;
+        public string ContentFill
+        {
+            get { return contentFill; }
+            set
+            {
+                contentFill = value;
+                OnPropertyChanged(nameof(ContentFill));
+                btnNum.Content = contentFill;
+            }
+        }
+
+        private int sizeFill;
+
+        public int SizeFill
+        {
+            get { return sizeFill; }
+            set 
+            { 
+                sizeFill = value; 
+                btnNum.FontSize = sizeFill;
+            }
+        }
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
